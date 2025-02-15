303ad6074c1dea51825f8ce5aceb24ad85edb2a9
Xinwei_GH <xinwei.cheng.88@gmail.com>
2025-02-15 12:36:48 +0800

    updated terraform and YAML

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,36 @@ output "security_group" {
 }
 
 output "ecs_task_execution_role" {
-  value       = length(aws_iam_role.ecs_task_execution_role) > 0 ? aws_iam_role.ecs_task_execution_role[0].name : data.aws_iam_role.existing_iam_role.name
+  value       = try(aws_iam_role.ecs_task_execution_role[0].name, data.aws_iam_role.existing_iam_role.name)
   description = "IAM Role for ECS Task Execution"
+}
+
+output "ecs_cluster_name" {
+  value       = module.ecs.cluster_name
+  description = "ECS Cluster Name"
+}
+
+output "ecs_service_name" {
+  value       = module.ecs.services["xinwei-service"].name
+  description = "ECS Service Name"
+}
+
+output "ecs_task_definition" {
+  value       = module.ecs.services["xinwei-service"].task_definition_arn
+  description = "ECS Task Definition ARN"
+}
+
+output "ecr_repository_url" {
+  value       = try(aws_ecr_repository.ecr[0].repository_url, data.aws_ecr_repository.existing_ecr.repository_url)
+  description = "ECR Repository URL"
+}
+
+output "vpc_id" {
+  value       = aws_vpc.xinwei_vpc.id
+  description = "VPC ID"
+}
+
+output "region" {
+  value       = var.aws_region
+  description = "AWS Deployment Region"
 }
